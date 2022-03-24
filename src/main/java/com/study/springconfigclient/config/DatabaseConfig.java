e2ac43c3d1082040aab62318a0ae1fc43f37541d
seongsoo <seongsoo.park5@cj.net>
2022-03-24 13:15:20 +0900

    actuator spring cloud config 동적 적용

--- src/main/java/com/study/springconfigclient/config/DatabaseConfig.java
+++ src/main/java/com/study/springconfigclient/config/DatabaseConfig.java
@@ -1,9 +1,14 @@
 package com.study.springconfigclient.config;
 
+import com.zaxxer.hikari.HikariConfig;
 import com.zaxxer.hikari.HikariDataSource;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.boot.jdbc.DataSourceBuilder;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DataSourceTransactionManager;
@@ -14,10 +19,18 @@ import javax.sql.DataSource;
 @MapperScan(basePackages = "com.study.springconfigclient.mapper")
 @Configuration
 public class DatabaseConfig {
+    @Autowired
+    private ApplicationContext applicationContext;
+
     @Bean
     @ConfigurationProperties("spring.datasource.hikari")
+    public HikariConfig hikariConfig() {
+        return new HikariConfig();
+    }
+
+    @Bean
     public DataSource dataSource() {
-        return DataSourceBuilder.create().type(HikariDataSource.class).build();
+        return new HikariDataSource(hikariConfig());
     }
 
     @Bean("transactionManager")
@@ -25,5 +38,26 @@ public class DatabaseConfig {
         DataSourceTransactionManager transactionManager = new DataSourceTransactionManager();
         transactionManager.setDataSource(dataSource);
         return transactionManager;
+    }
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory() throws Exception {
+        SqlSessionFactoryBean factoryBean = new SqlSessionFactoryBean();
+        factoryBean.setDataSource(dataSource());
+        factoryBean.setMapperLocations(applicationContext.getResources("classpath:/mappers/*.xml"));
+        factoryBean.setTypeAliasesPackage("com.study.springconfigclient.domain");
+        factoryBean.setConfiguration(mybatisConfg());
+        return factoryBean.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSession() throws Exception {
+        return new SqlSessionTemplate(sqlSessionFactory());
+    }
+
+    @Bean
+    @ConfigurationProperties(prefix = "mybatis.configuration")
+    public org.apache.ibatis.session.Configuration mybatisConfg() {
+        return new org.apache.ibatis.session.Configuration();
     }
 }
--- src/test/java/com/study/springconfigclient/controller/HealthCheckControllerTest.java
+++ src/test/java/com/study/springconfigclient/controller/HealthCheckControllerTest.java
@@ -1,22 +1,19 @@
 package com.study.springconfigclient.controller;
 
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.restdocs.RestDocumentationContextProvider;
 import org.springframework.restdocs.RestDocumentationExtension;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 
 import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
-import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.documentationConfiguration;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.preprocessRequest;
 import static org.springframework.restdocs.operation.preprocess.Preprocessors.prettyPrint;
 import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
@@ -29,15 +26,18 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith({RestDocumentationExtension.class, SpringExtension.class})
 @AutoConfigureRestDocs
 @SpringBootTest
+@AutoConfigureMockMvc
 class HealthCheckControllerTest {
+    @Autowired
     private MockMvc mockMvc;
-
-    @BeforeEach
-    public void setUp(WebApplicationContext webApplicationContext,
-                      RestDocumentationContextProvider restDocumentation) {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
-                .apply(documentationConfiguration(restDocumentation)).build();
-    }
+//    private MockMvc mockMvc;
+//
+//    @BeforeEach
+//    public void setUp(WebApplicationContext webApplicationContext,
+//                      RestDocumentationContextProvider restDocumentation) {
+//        this.mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext)
+//                .apply(documentationConfiguration(restDocumentation)).build();
+//    }
 
     @Test
     public void Health_테스트() throws Exception {
